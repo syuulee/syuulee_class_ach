ca981cb2c580f1f46d3b002b47d33666a9868a0f
syuulee <yuyu00lee@gmail.com>
2022-10-11 13:38:33 +0900

    2022.10.11.

---
+++ src/portfolioDATA.js
@@ -0,0 +1,9 @@
+const PD = [
+    { id: 1, title: "pf01", color: ['#333', '#f00', '#369'], content: "first homepage", link: "syuulee.github.io/", anchor: "pf01" },
+    { id: 2, title: "pf02", color: ['#f00', '#00f'], content: "second homepage", link: "syuulee.github.io/", anchor: "pf02" },
+    { id: 3, title: "pf03", color: ['#16f', '#9f0'], content: "third homepage", link: "syuulee.github.io/", anchor: "pf03" },
+    { id: 4, title: "pf04", color: ['#999', '#99f'], content: "third homepage", link: "syuulee.github.io/", anchor: "pf04" },
+]
+
+
+export default PD; 
